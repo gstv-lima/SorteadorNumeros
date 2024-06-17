599cb5ec54decc5a397977113030124d0ee57d73
Gustavo de Lima <gustavodelima1900@gmail.com>
2024-06-16 23:51:32 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,22 @@
+function sortear(){
+    let qtdNum = parseInt(document.getElementById('quantidade').value);
+    let numInicial = parseInt(document.getElementById('de').value);
+    let numFinal = parseInt(document.getElementById('ate').value);
+    
+    let sorteados = [];
+    let numero;
+
+    for (let i=0 ; i<qtdNum ; i++){
+    let numero = seletorAleatorio(numInicial, numFinal);
+        sorteados.push(numero);
+    }
+    
+    let campo = document.getElementById('resultado');
+    campo.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+
+}
+
+function seletorAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+

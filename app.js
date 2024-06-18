3505e09b5c2fb4b7f6784dc64650dba09b1f9ebe
Gustavo de Lima <gustavodelima1900@gmail.com>
2024-06-18 19:55:46 -0300

    Finalização do código

--- app.js
+++ app.js
@@ -16,9 +16,27 @@ function sortear(){
     let campo = document.getElementById('resultado');
     campo.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
 
+    alterarBtn();
 }
 
 function seletorAleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function alterarBtn(){
+    botao = document.getElementById('btn-reiniciar');
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+}
+
+function reiniciar(){
+
+    botao.classList.remove('container__botao');
+    botao.classList.add('container__botao-desabilitado');
+
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    sorteados = []; 
+}
